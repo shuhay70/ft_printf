b3bdd8742013d50ff700487dbe4ae0546d6f96b9
Horie Shuhei <hshuhei@c2r7s5.42tokyo.jp>
2025-11-11 14:54:22 +0900

    abc

--- ft_putnbr_base_uninptr.c
+++ ft_putnbr_base_uninptr.c
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr_base_uninptr.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hshuhei <hshuhei@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/08 16:50:23 by hshuhei           #+#    #+#             */
-/*   Updated: 2025/11/11 14:50:47 by hshuhei          ###   ########.fr       */
+/*   Created: 2025/11/08 17:13:16 by hshuhei           #+#    #+#             */
+/*   Updated: 2025/11/11 14:52:25 by hshuhei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_putstr(char *str)
+int	ft_putnbr_base(uintptr_t n, char *base)
 {
-	int	i;
+	int	base_len;
+	int	count;
 
-	i = 0;
-	if (str == NULL)
-	{
-		write (1, (nil), 5)
-		return (5);
-	}
-	while (str[i])
-	{
-		write (1, &str[i], 1);
-		i++;
-	}
-	return (i);
+	base_len = 0;
+	count = 0;
+	while (base[base_len])
+		base_len++;
+	if (n >= base_len)
+		count += ft_putnbr_base(n / base_len, base);
+	count += write(1, &base[n % base_len], 1);
+	return (count);
 }
