e996e341da0da91712e0728eec277fc2e22a95ba
Horie Shuhei <hshuhei@c2r7s5.42tokyo.jp>
2025-11-13 17:38:34 +0900

    a

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hshuhei <hshuhei@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 16:12:49 by hshuhei           #+#    #+#             */
-/*   Updated: 2025/11/13 16:45:33 by hshuhei          ###   ########.fr       */
+/*   Updated: 2025/11/13 17:38:23 by hshuhei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int	ft_putnbr_base(long n, char *base);
 int	ft_format_check1(char character, va_list ap);
 int	ft_format_check2(char character, va_list ap);
 int	ft_format_check3(char character, va_list ap);
+int	ft_format_check4(char character, va_list ap);
 int	ft_putchar(char c);
 int	ft_putnbr_base_uintptr(uintptr_t n, char *base);
 
